ae9423210a665fb162272dcbfa0a09b0f0a1b056
lefti <jes.bergstrom@gmail.com>
2014-07-26 19:16:08 +0300

    2 files were missing

---
+++ cpp-breakout/Entities/Paddle.cpp
@@ -0,0 +1,48 @@
+#include "Paddle.hpp"
+
+
+Paddle::Paddle() { }
+
+void Paddle::setBodyAndSprite(b2World* world, float posX, float posY) {
+    // Define a body.
+    b2BodyDef paddleBodyDef;
+    paddleBodyDef.type = b2_dynamicBody;
+    paddleBodyDef.position.Set(posX / PTM_RATIO, posY / PTM_RATIO);
+
+    // Use the body definition to create the actual body instance.
+    paddleBody = world->CreateBody(&paddleBodyDef);
+    paddleBody->SetUserData(static_cast<Entity*>(this));
+
+    // Define shape.
+    b2PolygonShape paddleShape;
+    paddleShape.SetAsBox(HALF_WIDTH / PTM_RATIO, HALF_HEIGHT / PTM_RATIO);
+
+    // Define fixture.
+    b2FixtureDef paddleFixtureDef;
+    paddleFixtureDef.shape = &paddleShape;
+    paddleFixtureDef.density = 1.f;
+    paddleFixtureDef.friction = 0.f;
+    paddleFixtureDef.restitution = 0.1f;
+
+    // Create user data.
+    bUserData* bud = new bUserData;
+    bud->entityType = PADDLE;
+    paddleFixtureDef.userData = bud;
+
+    // Create fixture.
+    paddleBody->CreateFixture(&paddleFixtureDef);
+
+    if(texture.loadFromFile("data/images/paddle_128x24.png")) {
+        sprite.setTexture(texture);
+    }
+    sprite.setOrigin(sf::Vector2f(HALF_WIDTH, HALF_HEIGHT));
+    sprite.setPosition(sf::Vector2f(paddleBody->GetPosition().x * PTM_RATIO, paddleBody->GetPosition().y * PTM_RATIO));
+}
+
+void Paddle::update(sf::RenderWindow& window, sf::Time deltaTime) { }
+
+void Paddle::draw(sf::RenderWindow& window) {
+    sprite.setPosition(sf::Vector2f(paddleBody->GetPosition().x * PTM_RATIO, paddleBody->GetPosition().y * PTM_RATIO));
+
+    window.draw(sprite); 
+}
---
+++ cpp-breakout/GameStates/LevelIntroState.cpp
@@ -0,0 +1,131 @@
+#include "LevelIntroState.hpp"
+#include "../GameStateMachine/StateMachine.hpp"
+
+
+LevelIntroState::LevelIntroState(int levelNumber, StateMachine& machine, sf::RenderWindow& window, bool replace)
+    : GameState(levelNumber, machine, window, replace) {
+
+    currentLevel = levelNumber;
+
+    font.loadFromFile("data/fonts/centurygothic.ttf");
+
+    // Create the path for map.txt using argument levelNumber.
+    std::string levelNumberStr = std::to_string(currentLevel);
+    std::string levelStr = "Level ";
+    std::string fullStr = levelStr + levelNumberStr;
+
+    // Create texts.
+    levelNumberText.setString(fullStr);
+    levelNumberText.setFont(font);
+    levelNumberText.setCharacterSize(80);
+
+    getReadyText.setString("Get ready!");
+    getReadyText.setFont(font);
+    getReadyText.setCharacterSize(60);
+
+    // Center the texts.
+    sf::FloatRect levelNumberTextRect = levelNumberText.getLocalBounds();
+    levelNumberText.setOrigin(sf::Vector2f(levelNumberTextRect.left + levelNumberTextRect.width / 2, levelNumberTextRect.top + levelNumberTextRect.height / 2));
+    levelNumberText.setPosition(sf::Vector2f(m_window.getSize().x / 2, 250));
+
+    sf::FloatRect getReadyTextRect = getReadyText.getLocalBounds();
+    getReadyText.setOrigin(sf::Vector2f(getReadyTextRect.left + getReadyTextRect.width / 2, getReadyTextRect.top + getReadyTextRect.height / 2));
+    getReadyText.setPosition(sf::Vector2f(m_window.getSize().x / 2, 400));
+
+    // Set background.
+    currentLevelIntro.setFillColor(sf::Color(0, 0, 0));
+    currentLevelIntro.setSize(sf::Vector2f(m_window.getSize().x, m_window.getSize().y));
+
+    // Start off opaque (not transparent).
+    introAlpha = sf::Color(0, 0, 0, 255);
+    // Start off transparent.
+    introAlpha2 = sf::Color(0, 0, 0, 0);
+
+    // Fill the fader surface with black.
+    introFader.setFillColor(introAlpha);
+    introFader.setSize(sf::Vector2f(m_window.getSize().x, m_window.getSize().y));
+    introFader2.setFillColor(introAlpha2);
+    introFader2.setSize(sf::Vector2f(m_window.getSize().x, m_window.getSize().y));
+
+    std::cout << "<< LevelIntroState initialized >>" << std::endl;
+}
+
+void LevelIntroState::processEvents() {
+    sf::Event event;
+
+    while(m_window.pollEvent(event)) {
+
+        switch(event.type) {
+
+        case sf::Event::Closed:
+            state_machine.quit();
+            break;
+
+        case sf::Event::KeyPressed: {
+
+            switch(event.key.code) {
+
+            case sf::Keyboard::Space:
+                m_next = StateMachine::build<PlayState>(0, state_machine, m_window, true);
+                std::cout << "Pressed SPACE in IntroState." << std::endl;
+                break;
+
+            case sf::Keyboard::Up:
+                std::cout << "Pressed UP arrow." << std::endl;
+                break;
+
+            case sf::Keyboard::Escape:
+                state_machine.quit();
+                break;
+
+            default:
+                break;
+            }
+            break;
+        }
+
+        default:
+            break;
+        }
+    }
+}
+
+void LevelIntroState::update(sf::Time deltaTime) {
+    if(introAlpha.a > 0) {
+        introAlpha.a -= 3;
+    }
+
+    if(introAlpha.a == 0) {
+        alphaCounter = 1;
+    }
+
+    if(introAlpha2.a < 250 && alphaCounter == 1) {
+        introAlpha2.a += 3;
+    }    
+    introFader.setFillColor(introAlpha);
+
+    introFader2.setFillColor(introAlpha2);
+}
+
+void LevelIntroState::draw() {
+    m_window.clear();
+
+    m_window.draw(currentLevelIntro);
+    m_window.draw(levelNumberText);
+    m_window.draw(getReadyText);
+
+    // No need to draw the first fader if the fader background is transparent
+    if(introAlpha.a != 0)
+    {
+        m_window.draw(introFader);
+    }
+    // No need to draw the second fader if the fader background is opaque
+    if(introAlpha2.a != 255) {
+        m_window.draw(introFader2);
+    }
+    if(introAlpha2.a > 250) {
+        m_next = StateMachine::build<PlayState>(currentLevel, state_machine, m_window, true);
+    }
+
+    m_window.display();
+}
